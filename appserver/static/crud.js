fa5c231d1b26b2ee75939c89af375f638269d56d
Alan Ivarson <aivarson@splunk.com>
2020-09-09 16:25:03 -0400

    crud.js complete

--- appserver/static/crud.js
+++ appserver/static/crud.js
@@ -8,8 +8,11 @@ require([
 var tokens = mvc.Components.get('submitted');
 
 /* --- Search Reference --- */
-var updateSearch = mvc.Components.get('updateSearch');
 var inputCollectionSearch = mvc.Components.get('inputCollectionSearch');
+var updateSearch = mvc.Components.get('updateSearch');
+var createSearch = mvc.Components.get('createSearch');
+var deleteSearch = mvc.Components.get('deleteSearch');
+
 
 /* --- Table Reference --- */
 var taskCollectionTable = mvc.Components.get('inputCollectionTable');
@@ -78,13 +81,17 @@ UseCase2_Input.val(UseCase2_Val);
 UseCase3_Input.val(UseCase3_Val);
 Notes_Input.val(Notes_Val);
 _key_Input.val(_key_Val);
+} else if(e['field'] === 'Delete') {
+
+    tokens.set('delete_key_tok', e.data['row._key']);
+
+}
 
 $('form *').filter(':input').each(function(){
    var value = $(this).val();
    console.log(value);
 });
 
-}
 
  });
  
@@ -92,36 +99,81 @@ $(document).on('click', '#submitButton', function(e) {
 
 e.preventDefault();
 
-tokens.set('update_key_tok', _key_Input.val());
-tokens.set('update_CustomerName_tok', CustomerName_Input.val());
-tokens.set('update_CustomerPOCEmail_tok', CustomerPOCEmail_Input.val());
-tokens.set('update_CustomerPOCName_tok', CustomerPOCName_Input.val());
-tokens.set('update_LicenseSizeGB_tok', LicenseSizeGB_Input.val());
-tokens.set('update_SalesEngineerEmail_tok', SalesEngineerEmail_Input.val());
-tokens.set('update_SalesEngineerName_tok', SalesEngineerName_Input.val());
-tokens.set('update_DataSource1_tok', DataSource1_Input.val());
-tokens.set('update_DataSource2_tok', DataSource2_Input.val());
-tokens.set('update_DataSource3_tok', DataSource3_Input.val());
-tokens.set('update_DataSource4_tok', DataSource4_Input.val());
-tokens.set('update_DataSource5_tok', DataSource5_Input.val());
-tokens.set('update_UseCase1_tok', UseCase1_Input.val());
-tokens.set('update_UseCase2_tok', UseCase2_Input.val());
-tokens.set('update_UseCase3_tok', UseCase3_Input.val());
-tokens.set('update_Notes_tok', Notes_Input.val());
+    if(_key_Input.val() != '') {
+        /* --- this is an update --- */
+		tokens.set('update_key_tok', _key_Input.val());
+		tokens.set('update_CustomerName_tok', CustomerName_Input.val());
+		tokens.set('update_CustomerPOCEmail_tok', CustomerPOCEmail_Input.val());
+		tokens.set('update_CustomerPOCName_tok', CustomerPOCName_Input.val());
+		tokens.set('update_LicenseSizeGB_tok', LicenseSizeGB_Input.val());
+		tokens.set('update_SalesEngineerEmail_tok', SalesEngineerEmail_Input.val());
+		tokens.set('update_SalesEngineerName_tok', SalesEngineerName_Input.val());
+		tokens.set('update_DataSource1_tok', DataSource1_Input.val());
+		tokens.set('update_DataSource2_tok', DataSource2_Input.val());
+		tokens.set('update_DataSource3_tok', DataSource3_Input.val());
+		tokens.set('update_DataSource4_tok', DataSource4_Input.val());
+		tokens.set('update_DataSource5_tok', DataSource5_Input.val());
+		tokens.set('update_UseCase1_tok', UseCase1_Input.val());
+		tokens.set('update_UseCase2_tok', UseCase2_Input.val());
+		tokens.set('update_UseCase3_tok', UseCase3_Input.val());
+		tokens.set('update_Notes_tok', Notes_Input.val());
+} else {
+    	  /* --- this is new so create --- */
+        tokens.set('create_tok', 'true');
+		tokens.set('create_CustomerName_tok', CustomerName_Input.val());
+		tokens.set('create_CustomerPOCEmail_tok', CustomerPOCEmail_Input.val());
+		tokens.set('create_CustomerPOCName_tok', CustomerPOCName_Input.val());
+		tokens.set('create_LicenseSizeGB_tok', LicenseSizeGB_Input.val());
+		tokens.set('create_SalesEngineerEmail_tok', SalesEngineerEmail_Input.val());
+		tokens.set('create_SalesEngineerName_tok', SalesEngineerName_Input.val());
+		tokens.set('create_DataSource1_tok', DataSource1_Input.val());
+		tokens.set('create_DataSource2_tok', DataSource2_Input.val());
+		tokens.set('create_DataSource3_tok', DataSource3_Input.val());
+		tokens.set('create_DataSource4_tok', DataSource4_Input.val());
+		tokens.set('create_DataSource5_tok', DataSource5_Input.val());
+		tokens.set('create_UseCase1_tok', UseCase1_Input.val());
+		tokens.set('create_UseCase2_tok', UseCase2_Input.val());
+		tokens.set('create_UseCase3_tok', UseCase3_Input.val());
+		tokens.set('create_Notes_tok', Notes_Input.val());
+    	}
 
 });
 
-/* --- Search Jobs --- */
-updateSearch.on('search:done', function() {
+	/* --- Search Jobs --- */
+	updateSearch.on('search:done', function() {
 
 
     inputCollectionSearch.startSearch();
     $('form *').filter(':input').each(function(){
         $(this).val('');
+    	});
+
+
+	});
+
+	createSearch.on('search:done', function() {
+
+
+        inputCollectionSearch.startSearch();
+
+
+        $('form *').filter(':input').each(function(){
+            $(this).val('');
+   	 });
+
+
     });
+    
+    deleteSearch.on('search:done', function() {
 
 
-});
+    inputCollectionSearch.startSearch();
+
+
+    tokens.unset('delete_key_tok');
+
+
+	});
 
 });
 
