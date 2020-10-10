fbc71118ef1027dc8365f8c2d6600a8a248db1a5
Alan Ivarson <aivarson@splunk.com>
2020-10-09 23:35:27 -0400

    Update POV progress dashboard
    Add KPI for Ad-hoc Searches

---
+++ appserver/static/countdown_timer.js
@@ -0,0 +1,42 @@
+require([
+    'splunkjs/mvc',
+    'splunkjs/mvc/simplexml/ready!'
+], function (mvc) {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+        
+  // Access the "default" token model
+ //   var tokens = mvc.Components.get("default");      
+        
+  // Retrieve the value of a token $end_date$
+ //   var tokenValue = tokens.get("end_date");
+
+  let end_date = "Oct 30, 2020 00:00:00",
+      countDown = new Date(end_date).getTime(),
+      x = setInterval(function() {    
+
+        let now = new Date().getTime(),
+            distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          let headline = document.getElementById("headline"),
+              countdown = document.getElementById("countdown"),
+              content = document.getElementById("content");
+
+          headline.innerText = "POV is out of time!";
+          countdown.style.display = "none";
+          content.style.display = "block";
+
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
